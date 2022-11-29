73c52ff3700d0b1d62026ca7109a02b3bdd973eb
Gaffeurs <gafferus@gmail.com>
2022-11-29 23:12:17 +0100

    Init repo 2 digits

--- init_repo.py
+++ init_repo.py
@@ -5,18 +5,18 @@ import shutil
 
 def create_directories():
     for i in range(2, 26):
-        os.mkdir(rf".\day_{i}")
+        os.mkdir(rf".\day_{i:02d}")
 
 
 def copy_jupyter():
     for path_directory in glob.glob("./day*")[1:]:
-        shutil.copy(r".\day_1\day_1.ipynb", rf"{path_directory}{path_directory[1:]}.ipynb")
+        shutil.copy(r".\day_01\day_01.ipynb", rf"{path_directory}{path_directory[1:]}.ipynb")
 
 
 def create_readme():
     for i in range(1, 26):
-        with open(rf".\day_{i}\README.md", 'w') as f:
-            f.write(f"JOUR {i}")
+        with open(rf".\day_{i:02d}\README.md", 'w') as f:
+            f.write(f"JOUR {i:02d}")
 
 
 if __name__ == '__main__':
